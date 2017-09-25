ad3a783546731043869d64b8c2952329cb6ae97f
Zaizi <gc.zaizi@hotmail.com>
2017-09-25 13:16:52 +0800

    add decisionTree

--- tree/tree.py
+++ tree/tree.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 
 from math import log
+import operator
+import treePlotter
 
 
 def calcShannonEnt(dataSet):
@@ -10,9 +12,7 @@ def calcShannonEnt(dataSet):
         currentLabel = featVect[-1]
         if currentLabel not in labelCounts.keys():
             labelCounts[currentLabel] = 0
-            labelCounts[currentLabel] += 1
-        else:
-            labelCounts[currentLabel] += 1
+        labelCounts[currentLabel] += 1
         shannonEnt = 0.0
     for key in labelCounts:
         prob = float(labelCounts[key]) / numEntries
@@ -23,8 +23,8 @@ def calcShannonEnt(dataSet):
 
 def createDataSet():
     dataSet = [
-        [1,1,'yse'],
-        [1,1,'yse'],
+        [1,1,'yes'],
+        [1,1,'yes'],
         [1,0,'no'],
         [0,1,'no'],
         [0,1,'no']
@@ -64,3 +64,53 @@ def chooseBestFeatureToSplit(dataSet):
             bestFeature = i
     
     return bestFeature
+
+
+# 返回出现次数最多的分类
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+
+    return sortedClassCount[0][0]
+
+
+def createTree(dataSet, labels):
+    # print(dataSet)
+    classList = [example[-1] for example in dataSet]    # 包含数据集的所有类标签
+    # print(classList)
+    
+    # 类别完全相同则停止划分
+    if classList.count(classList[0]) == len(classList): # classList[0] 出现了 len(classList) 次
+        return classList
+    
+    # 遍历完所有特征时返回出现次数最多的
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    
+    # 得到列表包含的所有属性值
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    # print(bestFeat, featValues)
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+
+    return myTree
+
+
+if __name__ == '__main__':
+    myDat, labels = createDataSet()
+    myTree = createTree(myDat, labels)
+    # print(myTree)
+    # myTree['no surfacing'][3] = 'maybe'
+    treePlotter.createPlot(myTree)
+    # print(treePlotter.getNumLeafs(myTree))
+    # print(treePlotter.getTreeDepth(myTree))
