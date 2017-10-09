bf3e9d2f9d4bbffcc25ada683362cd1562011d90
Zaizi <gc.zaizi@hotmail.com>
2017-10-09 15:07:09 +0800

    finish logistic

---
+++ logistic/mat.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+
+# 命令行调用 matplotlib 画图
+import matplotlib
+matplotlib.use('Agg')
--- mat.py
+++ mat.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
 # 命令行调用 matplotlib 画图
-import matplotlib
-matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+plt.plot(range(1,10))
+plt.show()
